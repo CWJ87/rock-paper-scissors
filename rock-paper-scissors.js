5431e0717cb143aa31aef5c738f1a5f5ebb186cd
Culbert Willoughby <culbertw@gmail.com>
2024-02-15 17:36:51 -0600

    feat: Add play a single round for rock-paper-scissors
    A single round of rock paper scissors is now playable
    and will return the winner of the game or if a draw game occurred.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -17,3 +17,29 @@ function getComputerChoice() {
             return "scissors";
     }
 }
+
+// plays a single round of Rock Paper Scissors
+function playRound(playerSelection, computerSelection) {
+    
+    // player's and computer's selection formatted
+    const captalizedPlayerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    const capitalizedComputerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1).toLowerCase();
+    const lowerCasePlayerSelection = playerSelection.toLowerCase();
+
+    // determine winner based on player's and computer's selection
+    if (lowerCasePlayerSelection === computerSelection) {
+        return `Draw Game! You both chose ${capitalizedComputerSelection}`;
+    }
+    else {
+        const combinedSelection = `${lowerCasePlayerSelection} ${computerSelection}`;
+
+        switch (combinedSelection) {
+            case "rock scissors":
+            case "scissors paper":
+            case "paper rock":
+                return `You Win! ${captalizedPlayerSelection} beats ${capitalizedComputerSelection}`;
+            default:
+                return `You Lose! ${capitalizedComputerSelection} beats ${captalizedPlayerSelection}`;
+        }
+    }
+}
